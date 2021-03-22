b527e5d1430f5b076854affc0e1df5dc1094b302
thecybercomrade <thecybercomrade@gmail.com>
2021-03-22 11:48:44 +0530

    index file sections add commit

--- js/index.js
+++ js/index.js
@@ -1,20 +1,29 @@
-const secondSection = document.querySelector(".second-section");
-const box = document.querySelector(".box");
+// const secondSection = document.querySelector(".second-section");
+// const box = document.querySelector(".box");
 
-const options = {
+// const options = {
 
-};
+// };
 
-const observer = new IntersectionObserver((entries, observer) => {
-    entries.forEach(entry => {
-     if(entry.isIntersecting) {
-        entry.target.style.backgroundColor = "white";
-        box.style.animation = "fading 2s infinite"
-        console.log("in")
-     }else {
-        // entry.target.style.backgroundColor = "green";
-     }
-    });
-}, options);
+// const observer = new IntersectionObserver((entries, observer) => {
+//     entries.forEach(entry => {
+//      if(entry.isIntersecting) {
+//         entry.target.style.backgroundColor = "white";
+//         box.style.animation = "fading 2s infinite"
+//         console.log("in")
+//      }else {
+//         // entry.target.style.backgroundColor = "green";
+//      }
+//     });
+// }, options);
 
-observer.observe(secondSection);
+// observer.observe(secondSection);
+
+
+
+anime ({
+    targets : 'div.cards h1',
+    translateX: 0,
+    duration: 2000,
+    easing: 'easeOutSine',
+});
