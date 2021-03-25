b0915f29ae46f2297e7aadaaccd6d7e602a345a1
thecybercomrade <thecybercomrade@gmail.com>
2021-03-25 18:37:34 +0530

    NavBar with js changes commit

--- js/index.js
+++ js/index.js
@@ -19,11 +19,40 @@
 
 // observer.observe(secondSection);
 
-
-
-anime ({
-    targets : 'div.cards h1',
-    translateX: [600, 0],
-    duration: 1000,
-    easing: 'easeInOutQuad'
+anime({
+  targets: "div.cards h1",
+  translateX: [600, 0],
+  duration: 1000,
+  easing: "easeInOutQuad",
 });
+
+const navMenu = document.querySelector(".nav-links");
+const menuButton = document.querySelector("i.fas.fa-bars");
+const closeButton = document.querySelector("i.fas.fa-times");
+
+const mediaQuery = window.matchMedia("(max-width: 730px)");
+
+handleTabletChange = (e) => {
+  if (!e.matches) {
+    navMenu.style.display = "block";
+  } else {
+    navMenu.style.display = "none";
+    closeButton.style.display = "none";
+    menuButton.style.display = "block";
+  }
+};
+
+mediaQuery.addListener(handleTabletChange);
+handleTabletChange(mediaQuery);
+
+menuButton.onclick = () => {
+  navMenu.style.display = "block";
+  menuButton.style.display = "none";
+  closeButton.style.display = "block";
+};
+
+closeButton.onclick = () => {
+  navMenu.style.display = "none";
+  closeButton.style.display = "none";
+  menuButton.style.display = "block";
+};
